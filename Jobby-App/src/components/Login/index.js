c9e544537ab0eda92919207350c72b5baad94bc3
Thoyaj1718 <thoyajkumar1718@gmail.com>
2026-05-15 14:48:57 +0530

    fix blank lines between class members

--- Jobby-App/src/components/Login/index.js
+++ Jobby-App/src/components/Login/index.js
@@ -16,7 +16,9 @@ class Login extends Component {
   }
 
   onChangeUsername = event => this.setState({username: event.target.value})
+
   onChangePassword = event => this.setState({password: event.target.value})
+  
   onChangeEmail = event => this.setState({email: event.target.value})
 
   onSubmitSuccess = jwtToken => {
